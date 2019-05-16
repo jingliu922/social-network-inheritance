a03d66fe91e67ba823b03cb00c4f797f4ca9a00c
Jing Liu <jingliu.922@gmail.com>
2019-05-16 16:52:22 -0700

    update accounts.py and posts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,27 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.posts=[]
+        self.following=[]
     def add_post(self, post):
-        pass
+        
+        self.posts.append(post)
+        
+        
+        
 
     def get_timeline(self):
-        pass
+        
+        a_list=[]
+        for user in self.following:
+            a_list += user.posts
+        
+        return a_list
+        
 
-    def follow(self, other):
-        pass
+    def follow(self, other):        
+        
+        self.following.append(other)
